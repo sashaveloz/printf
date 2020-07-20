9a9412a1977dd6cde5a59b4142009dd9a17921dc
sashaveloz <2065@holbertonschool.com>
2020-07-20 23:35:45 +0000

    Initial commit

---
+++ holberton.h
@@ -0,0 +1,13 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <limits.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+int _printf(const char *format, ...);
+int _putchar(char c);
+
+#endef /*HOLBERTON_H*/

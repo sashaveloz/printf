3979afa9e200518631621892f6dcc2d15abeb567
sashaveloz <2065@holbertonschool.com>
2020-07-21 22:42:28 +0000

    Printf function

---
+++ _printf.c
@@ -0,0 +1,43 @@
+#include "holberton.h"
+
+/**
+  *
+  */
+
+int _printf(const char *format, ...)
+{
+	int s, v = 0;
+	va_list m;
+	int (*func)(va_list);
+
+	va_start(m, format);
+
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
+
+	for (s = 0; format[s] != '\0'; s++)
+	{
+		if (format[s] == '%')
+		{
+			func = get_spc(format[s] + 1);
+
+			if (func != NULL)
+			{
+				v = v + func(m);
+				s++;
+			}
+			else
+			{
+				_putchar(format[s]);
+				v++;
+			}
+		}
+		else
+		{
+			_putchar(format[s]);
+			v++;
+		}
+	}
+	va_end(m);
+	return (v);
+}
